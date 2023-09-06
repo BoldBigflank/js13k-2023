b355f2df638d165b3ecd824086d258fd53a4adfe
Alex Swan <alex.swan@roblox.com>
2023-09-06 11:30:43 -0500

    Door detail

--- src/puzzles/Door.ts
+++ src/puzzles/Door.ts
@@ -16,7 +16,7 @@ export const Door = (scene: BABYLON.Scene) => {
         new Vector3( 2, 2, 0),
         new Vector3( 1.74, 3, 0),
         new Vector3( 1, 3.74, 0),
-        new Vector3( 0, 4, 0),
+        new Vector3( 0, 4, 0), // Just to the top
         new Vector3( -1, 3.74, 0),
         new Vector3( -1.74, 3, 0),
         new Vector3( -2, 2, 0),
@@ -29,10 +29,22 @@ export const Door = (scene: BABYLON.Scene) => {
     ]
 
     const frameOutline = [
-        new Vector3(0.55, -0.1, 0),
-        new Vector3(0.55, 0, 0),
-        new Vector3(-0.55, 0, 0),
-        new Vector3(-0.55, -0.1, 0)
+        new Vector3(0.51, -0.1, 0),
+        new Vector3(0.51, 0, 0),
+        new Vector3(-0.51, 0, 0),
+        new Vector3(-0.51, -0.1, 0)
+    ]
+
+    const doorMiddle = [
+        new Vector3(0, 0, 0),
+        new Vector3(0, 4, 0)
+    ]
+
+    const handleShape = [
+        new Vector3(.1, 0, 0),
+        new Vector3(.2, 0, 0),
+        new Vector3(.2, 0.05, 0),
+        new Vector3(.1, 0.05, 0)
     ]
 
     const door = MeshBuilder.ExtrudeShape("door", {
@@ -57,5 +69,29 @@ export const Door = (scene: BABYLON.Scene) => {
     }, scene)
     frame.material = ColorMaterial("#888888", scene)
     frame.setParent(parent)
+
+    const middle = MeshBuilder.ExtrudeShape("frame", {
+        shape: frameOutline,
+        path: doorMiddle,
+        closeShape: true,
+        cap: BABYLON.Mesh.CAP_ALL
+    }, scene)
+    middle.material = ColorMaterial("#000000", scene)
+    middle.setParent(parent)
+
+    const handle = BABYLON.MeshBuilder.CreateLathe('handle', {
+        shape: handleShape,
+        radius: 1,
+        closed: true,
+        tessellation: 12, sideOrientation: BABYLON.Mesh.DOUBLESIDE
+    }, scene)
+    handle.material = ColorMaterial("#000000", scene)
+    handle.setParent(parent)
+    handle.rotation.x = Math.PI / 2
+    handle.position = new Vector3(-0.5, 1, -0.55)
+
+    const handle2 = handle.clone("handle2")
+    handle2.position.x *= -1
+
     return parent
 }
