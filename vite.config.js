82ebe7595359af54450f53f5ff84e2261899b44b
Alex Swan <alex@bold-it.com>
2025-10-23 13:25:29 -0500

    yarn/l13

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+    base: '',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        },
+    },
+    server: {
+        allowedHosts: [
+            'localhost',
+            '127.0.0.1',
+            '0.0.0.0',
+            'ada3d69471bb.ngrok-free.app',
+            'together-horribly-lamprey.ngrok-free.app',
+        ]
+    },
+    build: {
+        rollupOptions: {
+            external: ['babylon'],
+        },
+    },
+})
